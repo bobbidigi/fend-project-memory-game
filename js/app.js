cf333cfc74da09998383f325997a9a4acca85f2e
RobertAllen <bobbidigi34@gmail.com>
2018-05-23 00:12:18 -0400

    add click listener by iterating over nodelist

--- js/app.js
+++ js/app.js
@@ -13,12 +13,12 @@ shuffle(cards);
 
 for (let i = 0; i <= cards.length - 1; i++) {
     const card = document.createElement('LI');
-    card.classList.add("card");
+    card.classList.add('card');
     const shape = document.createElement('I');
-    shape.classList.add("fa");
+    shape.classList.add('fa');
     shape.classList.add(cards[i]);
     card.appendChild(shape);
-    document.querySelector(".deck").appendChild(card);
+    document.querySelector('.deck').appendChild(card);
 }
 
 
@@ -39,13 +39,36 @@ function shuffle(array) {
 }
 
 
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+
+//set up the event listener for a card. If a card is clicked:
+
+// iterate through node list  from https://stackoverflow.com/a/21917404/6362721
+const cardNodeList = document.querySelectorAll('.card');
+console.log(cardNodeList);
+for (var i = 0; i < cardNodeList.length; i++) {
+    cardNodeList[i].addEventListener('click', clickFunction);
+}
+
+function clickFunction() {
+    console.log(this);
+    showFunc(this);
+    // cardsOpen();
+}
+
+function showFunc(elem) {
+    elem.classList.add('show');
+    elem.classList.add('open');
+}
+
+function cardsOpen() {
+    let openCardsList = [];
+}
+
+
+//  - display the card's symbol (put this functionality in another function that you call from this one)
+//  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+//  - if the list already has another card, check to see if the two cards match
+//    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+//     + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+//     + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
+//     + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
